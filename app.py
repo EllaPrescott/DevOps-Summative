2f44205c2571b8af149e1731ed216a28c78057d0
EllaPrescott <ella.prescott@hotmail.com>
2026-08-14 01:07:11 +0100

    Updated app.py with small change

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def dashboard():
     return render_template("index.html", data=data, new_transaction=new_transaction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
