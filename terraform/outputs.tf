4529e78c61e434a41938b710e361724c9ef4303e
EllaPrescott <ella.prescott@hotmail.com>
2026-08-15 22:30:35 +0100

    Fixed pipeline to test workflow

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 # Outputs for the EC2 instance ID and public IP address 
 
-output "instance_id" {
+output "ec2_instance_id" {
   value = aws_instance.flask.id
 }
 
-output "public_ip" {
+output "ec2_public_ip" {
   value = aws_instance.flask.public_ip
 }
